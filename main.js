3fff9208b6ba00f035679394bab48215ad9d1aaa
Immanuel <immanuel.lucasan@imagem.nl>
2026-02-03 15:39:29 +0100

    Lesson 4 (Complete)

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Bright Green Socks',
             image: './assets/images/socks_blue.jpg',
-            url: 'https://google.com/',
-            inventory: 8,
-            onSale: true
+            inStock: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes : ['S', 'M', 'L', 'XL' ]
         }
     }
 })
