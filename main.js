fe824c153309d42cce9df6ad9983f537b6132fdd
Immanuel <immanuel.lucasan@imagem.nl>
2026-02-03 14:55:34 +0100

    Lesson 3 (complete)

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Bright Green Socks'
+            description: 'Bright Green Socks',
+            image: './assets/images/socks_blue.jpg',
+            url: 'https://google.com/'
         }
     }
 })
