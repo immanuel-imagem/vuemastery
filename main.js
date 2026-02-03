701e6e397dca625eb0788e67021fd20ab2912f08
Immanuel <immanuel.lucasan@imagem.nl>
2026-02-03 14:44:40 +0100

    Lesson 2  (Complete)

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Bright Green Socks'
+        }
+    }
+})
