1e7fd6232ff16bd86a324e60f055653a9c569a88
Immanuel <immanuel.lucasan@imagem.nl>
2026-02-04 09:31:51 +0100

    Level 7 (complete)

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
-            ],
-            sizes : ['S', 'M', 'L', 'XL' ]
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
         }
     },
     methods: {
@@ -19,9 +19,6 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
-        },
-        removeFromCart() {
-            this.cart -= 1
         }
     }
 })
